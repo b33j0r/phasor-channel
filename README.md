bed3332704d9a6cb733ae0f5b17bae1973876b39
Brian Jorgensen <brian.jorgensen@gmail.com>
2025-10-17 13:12:50 -0600

    merge broadcast and etc from phasor-ecs#monorepo

---
+++ README.md
@@ -0,0 +1,8 @@
+# `phasor-channel`: simple mpsc and broadcast channels for zig
+
+This is part of [`phasor-ecs`](http://github.com/b33j0r/phasor-ecs). It provides simple channel implementations
+in multiple-producer/single-consumer and broadcast variants.
+
+## Zig's new IO, Queue, and async features
+
+I will probably rewrite this when the `IO`, `Queue`, and `async` features land in zig.
--- build.zig
+++ build.zig
@@ -1,14 +1,26 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const phasor_channel_module = b.addModule("phasor-channel", .{
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const phasor_channel_mod = b.addModule("phasor-channel", .{
         .root_source_file = b.path("src/root.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const phasor_channel_tests_mod = b.addModule("phasor_channel_tests", .{
+        .root_source_file = b.path("tests/tests.zig"),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{
+            .{ .name = "phasor-channel", .module = phasor_channel_mod },
+        },
     });
 
     const phasor_channel_tests = b.addTest(.{
-        .root_module = phasor_channel_module,
+        .root_module = phasor_channel_tests_mod,
     });
     const run_phasor_channel_tests = b.addRunArtifact(phasor_channel_tests);
     const test_step = b.step("test", "Run tests");
